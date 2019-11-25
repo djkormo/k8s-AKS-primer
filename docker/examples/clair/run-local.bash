49e919e4bb2865415a61237e50474c36d9ae36e5
Twoje imię i nazwsko <djkormo@gmail.com>
2019-11-25 15:42:06 +0100

    corrections

--- docker/examples/clair/run-local.bash
+++ docker/examples/clair/run-local.bash
@@ -5,6 +5,6 @@ docker run -ti \
   --name clair-scanner \
   --net clair \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /home/scanner-logs:. \
+  --mount src='pwd',target=/home/scanner-logs/,type=bind \
   djkormo/clair-scanner:latest /bin/bash
   
