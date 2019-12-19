16a46330c91f4284c610bb208be3ff5850e4880d
Krzysztof Pudłowski <kpudlowski@plus.pl>
2019-12-19 14:41:31 +0100

    correcting

--- install/scripting-cluster.bash
+++ install/scripting-cluster.bash
@@ -13,10 +13,18 @@ kubectl get ns
 kubectl get deploy,sts,svc,configmap,secret -n default -o yaml --export > default.yaml
 
 
-# for a single namespace in multiple yaml files 
+# for a single namespace (here = default) in multiple yaml files 
 
-for n in $(kubectl get -o=name pvc,configmap,serviceaccount,secret,ingress,service,deployment,statefulset,hpa,job,cronjob)
+for n in $(kubectl get -o=name pvc,configmap,serviceaccount,secret,ingress,service,deployment,statefulset,hpa,job,cronjob --namespace = default)
 do
     mkdir -p $(dirname $n)
     kubectl get -o=yaml --export $n > $n.yaml
 done
+
+
+# next try 
+ for n in $(kubectl get -o=name pvc,configmap,ingress,service,secret,deployment,statefulset,hpa,job,cronjob | grep -v 'secret/default-token'); 
+do  kubectl get -o=yaml --export $n > $(dirname $n)_$(basename $n).yaml; done
+
+
+
