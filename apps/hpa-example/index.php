1d59b3551f1bfdd659fc23a91920bb5ff4e8d9e5
KP <djkormo@gmail.com>
2019-12-24 14:00:08 +0100

    adding pi to index.php

--- apps/hpa-example/index.php
+++ apps/hpa-example/index.php
@@ -1,7 +1,23 @@
 <?php
+  $pi = 4; $top = 4; $bot = 3; $minus = TRUE;
+  $accuracy = 1000000;
+
+  for($i = 0; $i < $accuracy; $i++)
+  {
+    $pi += ( $minus ? -($top/$bot) : ($top/$bot) );
+    $minus = ( $minus ? FALSE : TRUE);
+    $bot += 2;
+  }
+  print "Pi ~=: " . $pi;
+  
   $x = 0.0001;
   for ($i = 0; $i <= 100000000; $i++) {
     $x += sqrt($x);
   }
   echo "OK!";
+  
+  
+  
+  
+  
 ?>
