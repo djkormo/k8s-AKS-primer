4d6687abe0b23787d9ccaed38f01b324c4dddd5e
KP <djkormo@gmail.com>
2020-01-06 15:59:40 +0100

    adding php-hello app

---
+++ apps/php-hello/src/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "Hello, World from Docker! <br>";
+echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+echo "<br> ";
+echo " Displayed at: ";
+echo gethostname();
+echo "<br>";
+echo "Application version : ";
+echo $_ENV["VERSION"] . '     !';
+echo "<br>";
+
+
+phpinfo(INFO_ENVIRONMENT);
+
+?>
